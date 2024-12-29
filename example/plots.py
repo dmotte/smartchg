e5e2af54da31d0a5408ff277074b92093ed03f87
dmotte <37443982+dmotte@users.noreply.github.com>
2024-12-29 03:29:58 +0100

    Plot based on rate values

--- example/plots.py
+++ example/plots.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 
 import argparse
+import csv
 import sys
 
 from contextlib import ExitStack
+from datetime import datetime as dt
+from typing import TextIO
+
+import plotly.express as px
+
+
+def load_data(file: TextIO):
+    '''
+    Loads data from a CSV file
+    '''
+    data = list(csv.DictReader(file))
+
+    for x in data:
+        yield {'date': dt.strptime(x['date'], '%Y-%m-%d').date()} | \
+            {k: float(x[k]) for k in ['days', 'rate', 'pred', 'offset',
+                                      'upper', 'lower', 'center', 'simil']}
 
 
 def main(argv=None):
@@ -19,7 +36,12 @@ def main(argv=None):
                         help='Input file. If set to "-" then stdin is used '
                         '(default: -)')
 
-    # TODO flags
+    parser.add_argument('-r', '--plot-rate', action='store_true',
+                        help='Generate plot based on rate values')
+    parser.add_argument('-o', '--plot-offset', action='store_true',
+                        help='Generate plot based on offset values')
+    parser.add_argument('-s', '--plot-simil', action='store_true',
+                        help='Generate plot based on simil values')
 
     args = parser.parse_args(argv[1:])
 
@@ -28,8 +50,30 @@ def main(argv=None):
     with ExitStack() as stack:
         file_in = (sys.stdin if args.file_in == '-'
                    else stack.enter_context(open(args.file_in, 'r')))
+        data = list(load_data(file_in))
 
-    # TODO plots
+    if args.plot_rate:
+        fig = px.line(
+            data,
+            x='date',
+            y=['rate', 'pred', 'upper', 'lower', 'center'],
+            template='plotly_dark',
+            title='Rate values',
+        )
+        fig.add_vline(
+            annotation_text='today',
+            x=dt.combine(data[-1]['date'], dt.min.time()).timestamp() * 1000,
+            line_color='#0cc',
+        )
+        fig.show()
+
+    if args.plot_offset:
+        pass  # TODO
+        fig.show()
+
+    if args.plot_simil:
+        pass  # TODO
+        fig.show()
 
     return 0
 
